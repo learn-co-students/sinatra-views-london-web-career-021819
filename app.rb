0a3061c93fa769de21d5ccec18225c608a52cc6d
github username <github email address>
2019-03-11 09:52:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# resp = Rack::Response.new
+	# req = Rack::Request.new
+
 	get '/' do
-		
+		erb :index
+	end
+
+	get "/info" do
+		erb :info
 	end
 end
